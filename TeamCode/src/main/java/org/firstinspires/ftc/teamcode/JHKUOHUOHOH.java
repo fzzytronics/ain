83f661741aaaa1160461e7b1ba77b1a6dc0cfd47
qiwiqameleonqameleon <remag3807@gmail.com>
2024-03-18 17:44:44 -0500

    ...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JHKUOHUOHOH.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JHKUOHUOHOH.java
@@ -4,8 +4,7 @@ import com.arcrobotics.ftclib.kinematics.Odometry;
 import com.arcrobotics.ftclib.geometry.Pose2d;
 import com.arcrobotics.ftclib.kinematics.Odometry;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-
-public class JHKUOHUOHOH {
+public class JHKUOHUOHOH<telemetry> {
     ///BRUHBRUH
     //MEWMEW
     public void Pose2d(double x,
@@ -15,10 +14,16 @@ public class JHKUOHUOHOH {
         double getHeading;
     }
 
-    public void telemetry {
-        telemetry.addData("X Position (in)",Pose2d.getX());
-        telemetry.addData("Y Position (in)",Pose2d.getY());
-        telemetry.addData("Theta (deg)",Math.toDegrees(Pose2d.getTheta()));
-        telemetry.update();
+    private Pose2d getPoseMeters() {
+        getPoseMeters().getX();
+        getPoseMeters().getY();
+        getPoseMeters().getHeading();
+        return null;
+    }
+
+    private void telemetry {
+        telemetry.addData ("getX"), Pose2d.getX();
+        telemetry.addData ("getY"), Pose2d.getY();
+        telemetry.addData ("getAngle"), Pose2d.getHeading();
 }
 }
