6bc1e31349832d529e400dd9decd00ef84ef0174
r3ds0und <johnathanbarbal1@gmail.com>
2024-03-18 17:30:47 -0400

    gjjg

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JHKUOHUOHOH.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JHKUOHUOHOH.java
@@ -1,7 +1,43 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@Autonomous
 public class JHKUOHUOHOH {
-    ///BRUHBRUH
-    //MEWMEW
-    import
+
+    DcMotorEx front_left = (DcMotorEx) hardwareMap.get(DcMotor.class, "front_left");
+    DcMotorEx front_right = (DcMotorEx)hardwareMap.get(DcMotor.class, "front_right");
+    DcMotorEx back_left = (DcMotorEx)hardwareMap.get(DcMotor.class, "back_left");
+    DcMotorEx back_right = (DcMotorEx)hardwareMap.get(DcMotor.class, "back_right");
+
+
+    public class OdometrySubsystem extends SubsystemBase {
+        public class Pose2d {
+            public int getWheelPositions() {
+                int wheelPositions = 0;
+                return wheelPositions;
+                wheelPositions = new MecanumDriveWheelPositions(
+                       back_left.getCurrentPosition(), back_right.getCurrentPosition();
+            }
+
+            }
+            }
+
+
+    public Pose2d getInstance() {
+
+
+        private class MecanumDriveWheelPositions {
+            public MecanumDriveWheelPositions(Object distance, Object distance1, Object p2) {
+            }
+        }
+    }
 }
+
+
