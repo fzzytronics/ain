f62fbca3365ca9c6888712f435f312a647bd31e2
Pablo Guzman <pabloguz72562@gmail.com>
2024-03-18 17:02:04 -0500

    fullcomm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/help.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/help.java
@@ -4,6 +4,7 @@ package org.firstinspires.ftc.teamcode;
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.OdometrySubsystem;
 import com.arcrobotics.ftclib.command.PurePursuitCommand;
+import com.arcrobotics.ftclib.geometry.Pose2d;
 import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.arcrobotics.ftclib.geometry.Translation2d;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
@@ -97,6 +98,7 @@ public class help extends CommandOpMode {
 
         // Set distance per pulse for encoders
         double ticksToInches = WHEEL_DIAMETER * Math.PI / TICKS_PER_INCH;
+
         encoderLeft.setDistancePerPulse(ticksToInches);
         encoderRight.setDistancePerPulse(ticksToInches);
         encoderCenter.setDistancePerPulse(ticksToInches);
@@ -111,7 +113,6 @@ public class help extends CommandOpMode {
 
         // Create odometry subsystem
         OdometrySubsystem odometry = new OdometrySubsystem(holOdom);
-
         // Initial odometry update
         odometry.update();
 
@@ -123,7 +124,6 @@ public class help extends CommandOpMode {
                 new GeneralWaypoint(200, 0, 0.8, 0.8, 30),
                 new EndWaypoint(400, 0, 0, 0.5, 0.5, 30, 0.8, 1)
         );
-
         // Schedule the command
     }
 }
