fa3ac2e3f0c25767eabff5381183f864458739fa
qiwiqameleonqameleon <remag3807@gmail.com>
2024-03-19 23:02:09 -0500

    ...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Justin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Justin.java
@@ -15,9 +15,9 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Autonomous
 public class Justin extends LinearOpMode {
-    private Motor.Encoder m_backLeftEncoder;
-    private Motor.Encoder m_backRightEncoder;
-    private Motor.Encoder right_encoder;
+    private Motor.Encoder encoderLeft;
+    private Motor.Encoder encoderRight;
+    private Motor.Encoder encoderCenter;
     private ServoEx m_gyro;
 
     public static class OdometrySubsystem extends SubsystemBase {
@@ -47,7 +47,7 @@ public class Justin extends LinearOpMode {
             MotorEx backRight = new MotorEx (hardwareMap, "back_right");
             MotorEx encoderLeft = new MotorEx(hardwareMap, "left_encoder");
             MotorEx encoderRight = new MotorEx(hardwareMap, "right_encoder");
-            MotorEx encoderPerp = new MotorEx(hardwareMap, "center_encoder");
+            MotorEx encoderCenter = new MotorEx(hardwareMap, "center_encoder");
         }
         initializeOdometry();
 
@@ -64,10 +64,10 @@ public class Justin extends LinearOpMode {
 
     @NonNull
     private MecanumDriveWheelSpeeds getMecanumDriveWheelSpeeds() {
-        Motor.Encoder left_encoder = null;
+        Motor.Encoder encoderLeft = null;
         MecanumDriveWheelSpeeds wheelSpeeds = new MecanumDriveWheelSpeeds(
-                left_encoder.getRate(), right_encoder.getRate(),
-                m_backLeftEncoder.getRate(), m_backRightEncoder.getRate()
+                encoderLeft.getRate(), encoderCenter.getRate(),
+                encoderLeft.getRate(), encoderRight.getRate()
         );
         return wheelSpeeds;
     }
@@ -81,25 +81,25 @@ public class Justin extends LinearOpMode {
 
         MotorEx encoderLeft = new MotorEx(hardwareMap, "front_left");
         MotorEx encoderRight = new MotorEx(hardwareMap, "front_right");
-        MotorEx encoderPerp = new MotorEx(hardwareMap, "back_right");
+        MotorEx encoderCenter = new MotorEx(hardwareMap, "back_right");
         // Initialize other motors and encoders...
     }
 
     private void initializeOdometry() {
         MotorEx encoderLeft = new MotorEx(hardwareMap, "left_encoder");
         MotorEx encoderRight = new MotorEx(hardwareMap, "right_encoder");
-        MotorEx encoderPerp = new MotorEx(hardwareMap, "center_encoder");
+        MotorEx encoderCenter = new MotorEx(hardwareMap, "center_encoder");
 
         // Set distance per pulse for encoders
         encoderLeft.setDistancePerPulse(TICKS_TO_INCHES);
         encoderRight.setDistancePerPulse(TICKS_TO_INCHES);
-        encoderPerp.setDistancePerPulse(TICKS_TO_INCHES);
+        encoderCenter.setDistancePerPulse(TICKS_TO_INCHES);
 
         // Create the odometry object
         HolonomicOdometry holOdom = new HolonomicOdometry(
                 encoderLeft::getDistance,
                 encoderRight::getDistance,
-                encoderPerp::getDistance,
+                encoderCenter::getDistance,
                 TRACKWIDTH, CENTER_WHEEL_OFFSET
         );
 
