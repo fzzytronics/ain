9932987a9c0e31427e86f2989067934a0f413c05
r3ds0und <johnathanbarbal1@gmail.com>
2024-04-03 16:51:04 -0400

    gjjg

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Justin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Justin.java
@@ -70,7 +70,6 @@ public class Justin extends LinearOpMode {
         }
     }
 
-    @NonNull
     private MecanumDriveWheelSpeeds getMecanumDriveWheelSpeeds() {
         MecanumDriveWheelSpeeds wheelSpeeds = new MecanumDriveWheelSpeeds(
                 encoderLeft.getRate(), encoderCenter.getRate(),
