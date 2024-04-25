dc2262d7d596160cc77efd8511d1827922be6245
qiwiqameleonqameleon <remag3807@gmail.com>
2024-04-25 16:55:04 -0500

    ...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Justin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Justin.java
@@ -29,9 +29,38 @@ import com.arcrobotics.ftclib.kinematics.HolonomicOdometry;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.OdometrySubsystem;
+import com.arcrobotics.ftclib.command.PurePursuitCommand;
+import com.arcrobotics.ftclib.drivebase.HDrive;
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.geometry.Pose2d;
+import com.arcrobotics.ftclib.geometry.Translation2d;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.kinematics.DifferentialOdometry;
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.MecanumDriveKinematics;
+import com.arcrobotics.ftclib.purepursuit.Path;
+import com.arcrobotics.ftclib.purepursuit.waypoints.EndWaypoint;
+import com.arcrobotics.ftclib.purepursuit.waypoints.GeneralWaypoint;
+import com.arcrobotics.ftclib.purepursuit.waypoints.StartWaypoint;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.arcrobotics.ftclib.kinematics.HolonomicOdometry;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 
-@Disabled
+
 @Autonomous
+@Override
 public class Justin extends LinearOpMode {
-//mnb,jhvuyuctogf
+
+    public static final double TRACKWIDTH = 14.7;
+    public static final double CENTER_WHEEL_OFFSET = -2.1;
+    public static final double WHEEL_DIAMETER = 2.0;
+    public static final double TICKS_PER_REV = 8192;
+    public static final double DISTANCE_PER_PULSE = Math.PI * WHEEL_DIAMETER / TICKS_PER_REV;
+
+    private MotorEx frontLeft, frontRight, backLeft, backRight;
+    private MecanumDrive drivetrain;
+    private HolonomicOdometry odometry;
+
 }
