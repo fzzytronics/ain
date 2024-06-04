f6db342dbaa83a600e5332d0e41bd2c7cf93ac67
qiwiqameleonqameleon <remag3807@gmail.com>
2024-06-04 08:58:25 -0500

    ...1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Justin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Justin.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.arcrobotics.ftclib.geometry.Pose2d;
 import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
