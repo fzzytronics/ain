b68a8c7a49f177682264532510c0d3d611c979e6
qiwiqameleonqameleon <remag3807@gmail.com>
2024-09-13 18:07:33 -0500

    ky

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChaosControl.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.MecanumDriveKinematics;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import com.arcrobotics.ftclib.kinematics.HolonomicOdometry;
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.hardware.motors.Motor.Encoder;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+    /** timer ///
+    we need map out coords ///
+    justin = cord
+            idk how speed (mulitplication mototrrrr)
+    we need calculus
+
+
+    **/
+    public class ChaosControl {
+public class Wawah{
+
+}
+
+
+}
