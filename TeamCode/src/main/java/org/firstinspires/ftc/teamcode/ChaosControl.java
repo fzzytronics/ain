6f6040f880e4a2027d3d3901f07e7dd67054bb68
qiwiqameleonqameleon <remag3807@gmail.com>
2024-09-14 18:49:58 -0500

    ky

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChaosControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChaosControl.java
@@ -17,10 +17,30 @@ import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 
 
     **/
+    @Autonomous
     public class ChaosControl {
-public class Wawah{
+        private ElapsedTime timer = new ElapsedTime(); // Declare timer as a class member
+        @Override
+        public void runOpMode() {
+            timer.reset(); // Start the timer at the beginning of Autonomous
 
-}
+            waitForStart();
 
+            while (opModeIsActive()) {
+                double seconds = timer.seconds();
+                telemetry.addData("Time (seconds)", seconds);
+                telemetry.update();
+                double speedModifier = calculateSpeedModifier(seconds);
 
-}
+            }
+        }
+
+        private double calculateSpeedModifier(double seconds) {
+            //testing testing one two three testing testing one two three
+            if (timer.seconds() > 5) {
+                return 0.75; // Reduce speed after 5 seconds
+            } else {
+                return 1.0;  // Full speed initially
+            }
+        }
+    }
