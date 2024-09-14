6d315398de6c6ac575c671f42d7830ff65b41429
qiwiqameleonqameleon <remag3807@gmail.com>
2024-09-14 18:53:31 -0500

    ky

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChaosControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChaosControl.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -21,29 +23,28 @@ import org.firstinspires.ftc.teamcode.Justin2;
 
     **/
     @Autonomous
-    public class ChaosControl {
-        private ElapsedTime timer = new ElapsedTime(); // Declare timer as a class member
-        @Override
-        public void runOpMode() {
-            timer.reset(); // Start the timer at the beginning of Autonomous
+public class ChaosControl extends LinearOpMode {
+    private ElapsedTime timer = new ElapsedTime(); // Declare timer as a class member
+    @Override
+    public void runOpMode() {
+        timer.reset(); // Start the timer at the beginning of Autonomous
 
-            waitForStart();
+        waitForStart();
 
-            while (opModeIsActive()) {
-                double seconds = timer.seconds();
-                telemetry.addData("Time (seconds)", seconds);
-                telemetry.update();
-                double speedModifier = calculateSpeedModifier(seconds);
-
-            }
-        }
-
-        private double calculateSpeedModifier(double seconds) {
-            //testing testing one two three testing testing one two three
-            if (timer.seconds() > 5) {
-                return 0.75; // Reduce speed after 5 seconds
-            } else {
-                return 1.0;  // Full speed initially
-            }
+        while (opModeIsActive()) {
+            double seconds = timer.seconds();
+            telemetry.addData("Time (seconds)", seconds);
+            telemetry.update();
+            double speedModifier = calculateSpeedModifier(seconds);
         }
     }
+
+    private double calculateSpeedModifier(double seconds) {
+        //testing testing one two three testing testing one two three
+        if (timer.seconds() > 5) {
+            return 0.75; // Reduce speed after 5 seconds
+        } else {
+            return 1.0;  // Full speed initially
+        }
+    }
+}
